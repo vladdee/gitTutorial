bedcbbd104e437cb467299fd3974b7d4aa8e3dc1
Vladimir Bondarenko <vladdeboika@gmail.com>
2017-09-22 01:09:36 +0200

    working code for the lab

--- labb4B.py
+++ labb4B.py
@@ -1,44 +1,51 @@
-
-
-def true_excep(expr, dic, oper):
-    if oper == "AND": 
-        if (expr[0] == "true" or dic[expr[0]] == "true") and (expr[2] == "true" or dic[expr[2]] == "true"):
-            return "true"
-        else:
-            return "false"
-    elif oper == "OR":
-        if (expr[0] == "true" or dic[expr[0]] == "true") or (expr[2] == "true" or dic[expr[2]] == "true"):
-            return "true"
-        else:
-            return "false"
-    elif oper == "NOT":
-        if dic[expr[1]] == "true" or expr[1] == "true":
-            return "false"
-        else:
-            return "true"
-        
-        
 def is_list(expr):
     if isinstance(expr, list):
         return True
     else:
         return False
+    
         
-def op_checker(expr):
-    if len(expr) == 2:
-        if expr[0] == "NOT":
-            return "NOT"
-    if len(expr) == 3:
-        if expr[1] == "AND":
-            return "AND"
-        elif expr[1] == "OR":
-            return "OR"
+def dic_true_checker(expr, dic, index):
+    if isinstance(expr[index], str):
+        if expr[index] in dic:
+            if dic[expr[index]] == "true":
+                return "true"
+            else:
+                return "false"
+    else:
+        return "false"
         
 
 def interpret(expr, dic):
     if isinstance(expr, str):
-        return dic[expr]
-    elif op_checker(expr) == "NOT":
+        if expr == "true" or expr == "false":
+            return expr
+        else:
+            return dic[expr]
+    elif len(expr) == 2:
         if is_list(expr[1]):
-            return interpret(expr[1])
-        return true_excep(expr, dic, )
+            return interpret(["NOT", interpret(expr[1], dic)], dic)
+        elif (expr[1] == "true") or (dic_true_checker(expr, dic, 1) == "true"):
+            return "false"
+        elif (expr[1] == "false") or (dic_true_checker(expr, dic, 1) == "false"):
+            return "true"
+    else:
+        if expr[1] == "AND":
+            if (expr[0] == "true" or dic_true_checker(expr, dic, 0) == "true") and (expr[2] == "true" or dic_true_checker(expr, dic, 2) == "true"):
+                return "true"            
+            elif is_list(expr[0]) and (expr[2] == "true" or dic_true_checker(expr, dic, 2) == "true"):
+                return interpret([interpret(expr[0], dic), "AND", "true"], dic)
+            elif is_list(expr[2]) and (expr[0] == "true" or dic_true_checker(expr, dic, 0) == "true"):
+                return interpret(["true", "AND", interpret(expr[2], dic)], dic)
+            else:
+                return "false"
+        elif expr[1] == "OR":
+            if is_list(expr[0]):
+                return interpret([interpret(expr[0], dic), "OR", expr[2]], dic)
+            elif is_list(expr[2]):
+                return interpret([expr[0], "OR", interpret(expr[2], dic)], dic)
+            elif (expr[0] == "true" or dic_true_checker(expr, dic, 0) == "true") or (expr[2] == "true" or dic_true_checker(expr, dic, 2) == "true"):
+                return "true"
+            else:
+                return "false"
+        
