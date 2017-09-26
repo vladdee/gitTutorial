7bf58864933c4955c78e008b047c70e183e8efda
Vladimir Bondarenko <vladdeboika@gmail.com>
2017-09-26 15:07:29 +0200

    removed statement to check for list on both sides of op

--- labb4B.py
+++ labb4B.py
@@ -3,8 +3,8 @@ def is_list(expr):
         return True
     else:
         return False
-    
-        
+
+
 def dic_true_checker(expr, dic, index):
     if isinstance(expr[index], str):
         if expr[index] in dic:
@@ -14,7 +14,7 @@ def dic_true_checker(expr, dic, index):
                 return "false"
     else:
         return "false"
-        
+
 def not_doer(expr, dic):
     if is_list(expr[1]):
         return interpret(["NOT", interpret(expr[1], dic)], dic)
@@ -22,29 +22,25 @@ def not_doer(expr, dic):
         return "false"
     elif (expr[1] == "false") or (dic_true_checker(expr, dic, 1) == "false"):
         return "true"
-        
+
 def and_doer(expr, dic):
     if (expr[0] == "true" or dic_true_checker(expr, dic, 0) == "true") and (expr[2] == "true" or dic_true_checker(expr, dic, 2) == "true"):
-        return "true"            
+        return "true"
     elif is_list(expr[0]):
         return interpret([interpret(expr[0], dic), "AND", expr[2]], dic)
     elif is_list(expr[2]):
         return interpret([expr[0], "AND", interpret(expr[2], dic)], dic)
-    elif is_list(expr[0]) and is_list(expr[2]):
-        return interpret([interpret(expr[0], dic), "AND", interpret(expr[2], dic)], dic)
     else:
         return "false"
-            
+
 def or_doer(expr, dic):
     if is_list(expr[0]):
         return interpret([interpret(expr[0], dic), "OR", expr[2]], dic)
     elif is_list(expr[2]):
         return interpret([expr[0], "OR", interpret(expr[2], dic)], dic)
-    elif is_list(expr[0]) and is_list(expr[2]):
-        return interpret([interpret(expr[0], dic), "OR", interpret(expr[2], dic)], dic)
     elif (expr[0] == "true" or dic_true_checker(expr, dic, 0) == "true") or (expr[2] == "true" or dic_true_checker(expr, dic, 2) == "true"):
         return "true"
-        
+
 def interpret(expr, dic):
     if isinstance(expr, str):
         if expr == "true" or expr == "false":
@@ -58,4 +54,3 @@ def interpret(expr, dic):
             return and_doer(expr, dic)
         elif expr[1] == "OR":
             return or_doer(expr, dic)
-        
