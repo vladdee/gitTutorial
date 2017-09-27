a6ea3b2a2f148b979ccaa6ff45a2fca1ef0c0701
Vladimir Bondarenko <vladdeboika@gmail.com>
2017-09-27 06:57:02 +0200

    added the itterativ version

--- labb4A.py
+++ labb4A.py
@@ -1,13 +1,26 @@
+secrets_lower = "_."
+secrets_upper = " |"
+
+def split_it(seq):
+    hidden_l_und_dot = ""
+    hidden_u_bl_pipe = ""
+    for i in seq:
+        if i.islower() or i in secrets_lower:
+            hidden_l_und_dot  += i
+        elif i.isupper() or i in secrets_upper:
+            hidden_u_bl_pipe += i
+    return hidden_l_und_dot, hidden_u_bl_pipe
+
 def split_rec(seq):
     first_message = ""
     second_message = ""
     if not seq:
         return first_message, second_message
     elif seq[0] in secrets_lower or seq[0].islower():
-        first_message, second_message = split_rec_three(seq[1:])
+        first_message, second_message = split_rec(seq[1:])
         return seq[0] + first_message, second_message
     elif seq[0] in secrets_upper or seq[0].isupper():
-        first_message, second_message = split_rec_three(seq[1:])
+        first_message, second_message = split_rec(seq[1:])
         return first_message, seq[0] + second_message
     else:
-        return split_rec_three(seq[1:])
+        return split_rec(seq[1:])
