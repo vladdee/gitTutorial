a3299e3cc6a9b452c9b357929cd116e2bba66578
Vladimir Bondarenko <vladdeboika@gmail.com>
2017-09-27 06:44:19 +0200

    corrected version with true recursion

--- labb4A.py
+++ labb4A.py
@@ -1,52 +1,13 @@
-
-secrets_lower = "_."
-secrets_upper = " |"
-
-
-def split_it(seq):
-    hidden_l_und_dot = ""
-    hidden_u_bl_pipe = ""
-    for i in seq:
-        if i.islower() or i in secrets_lower:
-            hidden_l_und_dot  += i
-        elif i.isupper() or i in secrets_upper:
-            hidden_u_bl_pipe += i
-    return hidden_l_und_dot, hidden_u_bl_pipe
-
-
-
-def in_lower(seq):
-    if not seq:
-        return ""
-    elif seq[0].islower() or seq[0] in secrets_lower:
-        return seq[0] + in_lower(seq[1:])
-    else:
-        return in_lower(seq[1:])
-
-
-def in_upper(seq):
-    if not seq:
-        return ""
-    elif seq[0].isupper() or seq[0] in secrets_upper:
-        return seq[0] + in_upper(seq[1:])
-    else:
-        return in_upper(seq[1:])
-
-
 def split_rec(seq):
-    return in_lower(seq), in_upper(seq)
-
-
-def split_rec_two(seq, res_low = "", res_high = ""):
+    first_message = ""
+    second_message = ""
     if not seq:
-        return res_low, res_high
-    elif seq[0].islower() or seq[0] in secrets_lower:
-        res_low += seq[0]
-        return split_rec_two(seq[1:], res_low, res_high)
-
-    elif seq[0].isupper() or seq[0] in secrets_upper:
-        res_high += seq[0]
-        return split_rec_two(seq[1:], res_low, res_high)
-
+        return first_message, second_message
+    elif seq[0] in secrets_lower or seq[0].islower():
+        first_message, second_message = split_rec_three(seq[1:])
+        return seq[0] + first_message, second_message
+    elif seq[0] in secrets_upper or seq[0].isupper():
+        first_message, second_message = split_rec_three(seq[1:])
+        return first_message, seq[0] + second_message
     else:
-        return split_rec_two(seq[1:], res_low, res_high)
+        return split_rec_three(seq[1:])
