18f1a60250ae1524e74d51172f50caecde8a5926
Vladimir Bondarenko <vladdeboika@gmail.com>
2017-10-11 00:53:26 +0200

    finished

--- labb4B.py
+++ labb4B.py
@@ -1,56 +1,41 @@
-def is_list(expr):
-    if isinstance(expr, list):
+def logic_constant(expr):
+    if expr == "true" or expr == "false":
         return True
     else:
         return False
 
+def is_string(expr):
+    if isinstance(expr, str):
+        return True
+    else:
+        return False
 
-def dic_true_checker(expr, dic, index):
-    if isinstance(expr[index], str):
-        if expr[index] in dic:
-            if dic[expr[index]] == "true":
-                return "true"
-            else:
-                return "false"
-
-def not_doer(expr, dic):
-    if is_list(expr[1]):
-        return interpret(["NOT", interpret(expr[1], dic)], dic)
-    elif (expr[1] == "true") or (dic_true_checker(expr, dic, 1) == "true"):
+def NOT(value):
+    if value == "true":
         return "false"
-    elif (expr[1] == "false") or (dic_true_checker(expr, dic, 1) == "false"):
+    else:
         return "true"
 
-def and_doer(expr, dic):
-    if (expr[0] == "true" or dic_true_checker(expr, dic, 0) == "true") and (expr[2] == "true" or dic_true_checker(expr, dic, 2) == "true"):
+def OR(value0, value1):
+    if value0 == "true" or value1 == "true":
         return "true"
-    elif is_list(expr[0]):
-        return interpret([interpret(expr[0], dic), "AND", expr[2]], dic)
-    elif is_list(expr[2]):
-        return interpret([expr[0], "AND", interpret(expr[2], dic)], dic)
     else:
         return "false"
 
-def or_doer(expr, dic):
-    if is_list(expr[0]):
-        return interpret([interpret(expr[0], dic), "OR", expr[2]], dic)
-    elif is_list(expr[2]):
-        return interpret([expr[0], "OR", interpret(expr[2], dic)], dic)
-    elif (expr[0] == "true" or dic_true_checker(expr, dic, 0) == "true") or (expr[2] == "true" or dic_true_checker(expr, dic, 2) == "true"):
+def AND(value0, value1):
+    if value0 == "true" and value1 == "true":
         return "true"
     else:
         return "false"
 
 def interpret(expr, dic):
-    if isinstance(expr, str):
-        if expr == "true" or expr == "false":
-            return expr
-        else:
-            return dic[expr]
+    if logic_constant(expr):
+        return expr
+    elif is_string(expr):
+        return dic[expr]
     elif len(expr) == 2:
-        return not_doer(expr, dic)
-    else:
-        if expr[1] == "AND":
-            return and_doer(expr, dic)
-        elif expr[1] == "OR":
-            return or_doer(expr, dic)
+        return NOT(interpret(expr[1], dic))
+    elif expr[1] == "OR":
+        return OR(interpret(expr[0], dic), interpret(expr[2], dic))
+    elif expr[1] == "AND":
+        return AND(interpret(expr[0], dic), interpret(expr[2], dic))
